cdbeee631fe1a22875859410ddf588787d2ae770
Wes Hardaker <opensource@hardakers.net>
2023-09-07 02:10:10 -0700

    process-loader

---
+++ pyfsdb_viewer/dataloader/processloader.py
@@ -0,0 +1,90 @@
+import pyfsdb
+import shlex
+import tempfile
+import logging
+import time
+from queue import SimpleQueue as FifoQueue
+from concurrent.futures import ThreadPoolExecutor
+from subprocess import Popen, PIPE, STDOUT
+
+from . import DataLoader
+
+
+class ProcessLoader(DataLoader):
+    "Executes a process with a pipe to a new file and loads it at least in part"
+    def __init__(self, command, input_file_name):
+        super().__init__()
+
+        # save the command
+        if not isinstance(command, list):
+            command = shlex.split(command)
+        self.command = command
+
+        self.executor = None
+        self.queue = None
+        self.input_file_name = input_file_name
+        self.fsh = None
+
+        self.temp_file = tempfile.NamedTemporaryFile(delete=False)
+
+        self.run_pipe(command)
+
+    def run_pipe(self, command):
+        try:
+            p = Popen(self.command, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+
+            # run the specified command
+            input_file = open(self.input_file_name, "r").read().encode()
+
+            # this reads all data -- we need a better solution than this
+            # need to do parallel reading and writing of data
+            output_data = p.communicate(input=input_file)
+
+            self.debug(output_data, savefile="/tmp/debug-test.txt")
+            self.temp_file.write(output_data[0]) # save stdout to the file
+
+        except Exception as e:
+            self.debug(f"failed with {e}")
+            logging.error(f"failed with {e}")
+
+    @property
+    def name(self):
+        return self.temp_file.name
+
+    @property
+    def temp_file_handle(self):
+        return open(self.name, "r")
+
+    @property
+    def commands(self):
+        try:
+            return self.fsh.parse_commands()
+        except Exception:
+            return None
+
+    def load_data(self) -> None:
+        self.fsh = pyfsdb.Fsdb(file_handle=self.temp_file_handle)
+        self.rows = []
+
+    def load_more_data(self, current_rows, max_rows=128) -> None:
+        more_rows = []
+        for n, row in enumerate(self.fsh):
+            more_rows.append(row)
+            if max_rows and n == max_rows:
+                break
+        current_rows.extend(more_rows)
+        return more_rows
+
+    def column_names(self):
+        return self.fsh.column_names
+
+    def __iter__(self):
+        """Returns an iterator object for looping over from the current file."""
+        if not self.filename and not self.file_handle:
+            raise ValueError("No filename or handle currently available for reading")
+        # XXX: throw error on -1 parse
+        return self
+
+    def __next__(self):
+        return next(self.fsh)
+        
